730cb95fb1bdde8040c12a13a5d1bf9ff2fbc522
Alla Kuzminova <alchik@gmail.com>
2020-09-12 19:53:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,12 @@ def reverse_each_word(sentence)
   array = sentence.split
   counter = 0
   new_sentence = ""
-  array.each {|word|
+  array.collect {|word|
     new_sentence << word.reverse
     counter +=1
     if(counter < array.size)
       new_sentence << " "
     end
   }
-  new_sentence.strip
 end
 puts reverse_each_word("Hello there, and how are you?")
